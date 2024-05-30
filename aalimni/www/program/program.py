49582abed2472adef02cc360e84fa40b3edc5adf
yasminezzzz <yasmineslema9122001@gmail.com>
2024-05-31 00:58:27 +0200

    hello

--- aalimni/www/program/program.py
+++ aalimni/www/program/program.py
@@ -15,12 +15,21 @@ def get_context(context):
     # Récupérer les leçons
     LESSONS = db.sql("""
         SELECT name, number, percentchap, percentsubject, chap
-        FROM tablessons
+        FROM tablessons ;
     """, as_dict=True)
-    
+
     # Associer les leçons aux chapitres correspondants
     for chapter in CHAPTER:
         chapter['lessons'] = [lesson for lesson in LESSONS if lesson['chap'] == chapter['name1']]
     
     context.CHAPTER = CHAPTER
     context.LESSONS = LESSONS  # Si nécessaire pour d'autres utilisations
+
+    # Accès à la liste des leçons
+    lesson_names = [lesson['name'] for lesson in LESSONS]
+    lesson_numbers = [lesson['number'] for lesson in LESSONS]
+    lesson_percent_chap = [lesson['percentchap'] for lesson in LESSONS]
+    lesson_percent_subject = [lesson['percentsubject'] for lesson in LESSONS]
+    lesson_chap = [lesson['chap'] for lesson in LESSONS]
+    
+    # Vous pouvez utiliser ces listes comme nécessaire dans le reste de votre code
